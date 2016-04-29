ab1c2cf4bea8c04177f91248e1237417f5991cc0
medp <patrutiu_medeea@yahoo.com>
2016-04-29 16:12:03 +0300

    Css&JavaScript
    Design ,dropdown menu, validation

---
+++ SoundCloud/home.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function validateInput(x){
+  if (x == null || x == "") {
+       alert("Name must be filled out");
+       return false;
+   }
+   return true;
+}
