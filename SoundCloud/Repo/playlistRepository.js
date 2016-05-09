d71485b8795455bd52d26b79bbe5c5c8f593737a
medp <patrutiu_medeea@yahoo.com>
2016-05-09 19:05:20 +0300

    JQuery

--- SoundCloud/Repo/playlistRepository.js
+++ SoundCloud/Repo/playlistRepository.js
@@ -2,27 +2,28 @@
 (function(context,jQ) {
   'use strict';
 
-  
-  function genericRepository (url, cbSuccess, cbError) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-        cbSuccess(xhttp.responseText);
-      } else {
-        if (!!xhttp.status && xhttp.status !== 200) {
-          cbError(xhttp.responseText);
-        }
-      }
-    };
-    xhttp.open("GET", url , true);
-    xhttp.send();
-  };
-
-
-  function getPlaylist(cbSuccess, cbError) {
-    var url = "https://api.soundcloud.com/playlists?client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
-    return genericRepository(url, cbSuccess, cbError);
+  function genericRepo(success,error){
+    return jQ.ajax({
+      url: getUrl(),
+      dataType: "json",
+      type: 'GET',
+      jsonp:false,
+    })
+      .done(function(data) {
+        jQ("document").ready(success(data));
+      })
+      .fail(function( req, status, err ) {
+        alert("fail");
+      });
   }
 
-  context.repo.getPlaylist = getPlaylist;
+  function getType(){
+    return "GET";
+  }
+  function getUrl(){
+    var url = "https://api.soundcloud.com/playlists?client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea";
+    return url;
+  }
+
+  context.repo.genericRepo = genericRepo;
 })(myApp,jQuery);
--- SoundCloud/Service/playlistService.js
+++ SoundCloud/Service/playlistService.js
@@ -1,9 +1,10 @@
 (function(ctx) {
   'use strict';
+
   function getPlaylist(cbSuccess, cbError) {
     function getWithSuccess(data) {
       var playlists;
-      playlists =JSON.parse(data).map(function(playlist) {
+      playlists =data.map(function(playlist) {
           return new ctx.business.Playlist(playlist);
       });
       cbSuccess(playlists);
@@ -11,7 +12,7 @@
     function getWithError(data) {
       cbError(data);
     }
-    ctx.repo.getPlaylist(getWithSuccess, getWithError);
+    ctx.repo.genericRepo(getWithSuccess, getWithError);
   }
 
   ctx.service.getPlaylist = getPlaylist;
