747fe180a012a6ca383b2dedb2f73c00b091f552
medp <patrutiu_medeea@yahoo.com>
2016-05-27 18:46:36 +0300

    add

---
+++ SoundCloud-Angular/app/test/directive.test.js
@@ -0,0 +1,22 @@
+"use strict"
+
+beforeEach(module('myApp'));
+
+
+describe("Should test directive", function() {
+    var $compile,$rootScope;
+    beforeEach(module("../app/templates/spinner.html"));
+    beforeEach(inject(function(_$compile_,_$rootScope_){
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+    }));
+
+  it('Replaces the element with the appropriate content', function() {
+  // Compile a piece of HTML containing the directive
+  // var element = $compile("<pp-spinner></pp-spinner>")($rootScope);
+  // // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+  // $rootScope.$digest();
+  // // Check that the compiled element contains the templated content
+  // expect(element.html()).toContain("Loading...");
+});
+})
